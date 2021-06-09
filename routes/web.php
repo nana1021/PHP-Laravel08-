acece94986a283b489365e83cdb0abd91a5fc2cf
onogawa <nananananana21@gmail.com>
2021-06-09 07:02:29 +0000

    課題

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+Route::get('XXX','AAAController@bbb');
+
+Route::group(['prefix' => 'admin'], function() {
+Route::get('profile/create','Admin\ProfileController@add');
+Route::get('profile/edit','Admin\ProfileController@edit');
+});
