a62f402506a43241e9f468cc1621bf081fe72c50
onogawa <nananananana21@gmail.com>
2021-06-28 10:11:10 +0000

    メンタリング

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,17 +4,32 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
-    //
+    
     public function add()
     {
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+      
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      
+      unset($form['_token']);
+      unset($form['image']);
+      
         return redirect('admin/profile/create');
     }
 
